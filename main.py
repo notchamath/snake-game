69d4f29c8aa816f1b2aaf7ee0f45a92cc00e8ac5
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-24 12:50:16 -0400

    feat: make snake move forward

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("SNAKE GAME")
+screen.tracer(0)
+
 snake_body = []
+is_game_on = True
 
 for i in range(3):
     body_segment = Turtle("square")
     body_segment.color("white")
-    body_segment.setpos(x=i*-20, y=0)
+    body_segment.penup()
+    body_segment.goto(x=i*-20, y=0)
 
     snake_body.append(body_segment)
 
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for i in range(len(snake_body)-1, 0, -1):
+        snake_body[i].goto(snake_body[i-1].pos())
+    snake_body[0].forward(20)
 
 screen.exitonclick()
