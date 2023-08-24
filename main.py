34b4355c2721ee4f6f385dfedc55afa8fbd54306
Chamath Jayarathne <chamathsocial@gmail.com>
2023-08-24 14:09:48 -0400

    feat: Add food to game. Detect when snake eats the food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Screen
 import time
+from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 is_game_on = True
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +23,9 @@ screen.onkey(snake.right, "Right")
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
